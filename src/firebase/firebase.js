bb983dcb38285f800aa935fe67b78ab0bbef599c
Blargian <shaunstruwig.ss@gmail.com>
2021-02-22 09:06:40 +0200

    :sparkles: Completed Section 14: Firebase 101

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,53 +14,32 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().on('value',(snapshot)=>{
-    console.log(snapshot.val());
+// database.ref('expenses').once('value').then((snapshot)=>{
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot)=>{
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ... childSnapshot.val()
+//         });
+//     })
+
+//     console.log(expenses)
+// })
+
+// database.ref('expenses').on('value',(snapshot)=>{
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot)=>{
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ... childSnapshot.val()
+//         });
+//     });
+
+//     console.log(expenses);
+// })
+
+database.ref('expenses').on('child_changed',(snapshot)=>{
+    console.log(snapshot.key,snapshot.val());
 })
-
-database.ref().set({
-    name: 'Shaun',
-    age: 26,
-    isSingle: false,
-    stressLevel: 6,
-    job: {
-        title: 'Software developer',
-        company: 'Google',
-    },
-    location: {
-        city: 'Moscow',
-        country: 'Russian Federation'
-    },
-}).then(()=>{
-    console.log('Data is saved');
-}).catch((e)=> {
-    console.log('This failed.', e);
-});
-
-//database.ref().set('This is some data');
-
-database.ref('attributes').set({
-    height: 125,
-    weight: 70
-}).then(()=>{
-    console.log('Data is saved');
-}).catch((e)=>{
-    console.log('This failed.',e);
-});
-
-database.ref('attributes').remove().then(()=>{
-    console.log('removed');
-}).catch((e)=>{
-    console.log('failed',e);
-})
-
-database.ref().update({
-    'job/company': 'Amazon', 
-    'stressLevel': 9,
-    'location/city': 'Seattle'
-}).then(()=>{
-    console.log('Completed update action')
-}).catch((e)=>{
-    console.log('Something went wrong',e)
-})
-
