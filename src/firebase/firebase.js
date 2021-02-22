2448bf4386729677c02903dd4d93148c28162f59
Blargian <shaunstruwig.ss@gmail.com>
2021-02-22 21:37:22 +0200

    :construction: Integrating firebase into expensify

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,45 +1,10 @@
-import * as firebase from 'firebase'; //make a variable out of all named exports
+import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAAIpyASnSgaajeVxKfU2ga0mdKjmRzYqc",
-    authDomain: "expensify-cf09e.firebaseapp.com",
-    databaseURL: "https://expensify-cf09e-default-rtdb.firebaseio.com",
-    projectId: "expensify-cf09e",
-    storageBucket: "expensify-cf09e.appspot.com",
-    messagingSenderId: "586329035051",
-    appId: "1:586329035051:web:8c6ab03b6f08c35501356e"
   };
 
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-// database.ref('expenses').once('value').then((snapshot)=>{
-//     const expenses = [];
-
-//     snapshot.forEach((childSnapshot)=>{
-//         expenses.push({
-//             id: childSnapshot.key,
-//             ... childSnapshot.val()
-//         });
-//     })
-
-//     console.log(expenses)
-// })
-
-// database.ref('expenses').on('value',(snapshot)=>{
-//     const expenses = [];
-
-//     snapshot.forEach((childSnapshot)=>{
-//         expenses.push({
-//             id: childSnapshot.key,
-//             ... childSnapshot.val()
-//         });
-//     });
-
-//     console.log(expenses);
-// })
-
-database.ref('expenses').on('child_changed',(snapshot)=>{
-    console.log(snapshot.key,snapshot.val());
-})
+export { firebase, database as default }
