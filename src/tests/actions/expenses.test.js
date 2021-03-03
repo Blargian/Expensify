889957e0111a07912e61f1a8c6b2888a840d0906
Blargian <shaunstruwig.ss@gmail.com>
2021-03-03 16:46:03 +0200

    :sparkles: Added Firebase edit functionality and tests

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -4,10 +4,11 @@ import {
   startAddExpense, 
   addExpense,
   editExpense,
+  startEditExpense,
   removeExpense,
   setExpenses,
   startSetExpenses,
-  startRemoveExpense 
+  startRemoveExpense,
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
@@ -56,6 +57,25 @@ test('should setup edit expense action object', () => {
       note: 'New note value'
     }
   });
+});
+
+test('Should edit expense from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = { amount: 5};
+  store.dispatch(startEditExpense(id,updates)).then(()=>{
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    });
+
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot)=>{
+    expect(snapshot.val().amount).toBe(updates.amount);
+    done();
+  })
 });
 
 test('should setup add expense action object with provided values', () => {
