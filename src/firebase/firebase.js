6f9398c427b82125dee13d44560aad4d8129b453
Blargian <shaunstruwig.ss@gmail.com>
2021-03-03 14:41:15 +0200

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,16 @@
-import firebase from 'firebase';
+import * as firebase from 'firebase';
 
-const firebaseConfig = {
-  };
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
 const database = firebase.database();
 
-export { firebase, database as default }
+export { firebase, database as default };
